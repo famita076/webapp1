ffb7fdc04746e8950335629664d616130948899f
kelompok4 <129141184+famita076@users.noreply.github.com>
2023-12-04 13:38:17 +0000

    bismillahhh

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sqlalchemy import text
 list_jenis_kelamin = ['', 'Perempuan', 'Laki-laki'] 
 
 conn = st.connection("postgresql", type="sql", 
-                     url="postgresql://famitawibi20:y2abk9QcReHn@ep-patient-field-58242561.us-east-2.aws.neon.tech/web")
+                     url="postgresql://famitawibi20:y2abk9QcReHn@ep-patient-field-58242561.us-east-2.aws.neon.tech/web?options=endpoint%3Dep-patient-field-58242561")
 
 st.header('DATABASE SEBARAN ALUMNI MAHASISWA STATISTIKA BISNIS')
 page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
@@ -16,12 +16,12 @@ if page == "View Data":
 if page == "Edit Data":
     if st.button('Tambah Data'):
         with conn.session as session:
-            query = text('INSERT INTO sebaran_data (nama_mahasiswa, nrp_mahasiswa, jenis_kelamin, angkatan, alamat_domisili, email, handphone, sosmed, nama_instansi, jabatan, alamat_instansi)\
+            query = text('INSERT INTO sebaran_pekerjaan (nama_mahasiswa, nrp_mahasiswa, jenis_kelamin, angkatan, alamat_domisili, email, handphone, sosmed, nama_instansi, jabatan, alamat_instansi)\
                           VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11);')
             session.execute(query, {'1': '', '2': '', '3': '', '4': '', '5': '', '6': '', '7': '', '8': ' ', '9': '', '10': ' ', '11': ''})
             session.commit()
 
-    data = conn.query('SELECT * FROM sebaran_alumni ORDER By id;', ttl="0")
+    data = conn.query('SELECT * FROM sebaran_pekerjaan ORDER By id;', ttl="0")
     for _, result in data.iterrows():        
         id = result['id']
         nama_mahasiswa_lama = result["nama_mahasiswa"]
@@ -55,18 +55,19 @@ if page == "Edit Data":
                 with col1:
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
-                            query = text('UPDATE sebaran_alumni\
+                            query = text('UPDATE sebaran_pekerjaan\
                                           SET nama_mahasiswa=:1, nrp_mahasiswa=:2, jenis_kelamin=:3, angkatan=:4, \
                                           alamat_domisili=:5, email=:6, handphone=:7, sosmed=:8 , nama_instansi=:9,  jabatan=:10, alamat_instansi=:11 \
                                           WHERE id=:12;')
                             session.execute(query, {'1':nama_mahasiswa_baru, '2':nrp_mahasiswa_baru, '3':jenis_kelamin_baru, '4':angkatan_baru, 
                                                     '5':alamat_domisili_baru, '6':email_baru, '7':handphone_baru, '8':sosmed_baru, '9':nama_instansi_baru, '10': jabatan_baru, '11':alamat_instansi_baru, '12':id})
                             session.commit()
-                            st.experimental_rerun()
+                            st.rerun()
                 
                 with col2:
                     if st.form_submit_button('DELETE'):
-                        query = text(f'DELETE FROM sebaran_alumni WHERE id=:1;')
-                        session.execute(query, {'1':id})
-                        session.commit()
-                        st.experimental_rerun()
+                        with conn.session as session:
+                            query = text(f'DELETE FROM sebaran_pekerjaan WHERE id=:1')
+                            session.execute(query, {'1':id})
+                            session.commit()
+                            st.rerun()
